d6c3fadbfbaba1d3f6e2007387c25b82d6600a18
olegweb1981 <omykhaylovskiy@gmail.com>
2019-11-26 20:29:09 +0200

    responsiv site

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 
+//Navbar
+function showMenu() {
+
+  if (document.body.classList.contains('is-menu-visible'))
+      document.body.classList.remove('is-menu-visible');
+  else
+      document.body.classList.add('is-menu-visible');
+}
+
 //*region Shuffle
 var filters = $('.filters');
 var filterButtons = filters.find('button');
